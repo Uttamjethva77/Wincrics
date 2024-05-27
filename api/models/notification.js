e7a2e99f4ff2d3ecae9834716d1d0e029c743cb7
Uttam Jethva <95206154+Uttamjethva77@users.noreply.github.com>
2024-05-27 17:25:14 +0530

    Update notification.js

--- api/models/notification.js
+++ api/models/notification.js
@@ -3,11 +3,23 @@ const pool = require('../db/db');
 class Notification {
   static async create(notificationData) {
     const { email, whatsapp_number, name } = notificationData;
+    
+    // Check if email or whatsapp_number already exists
+    const checkQuery = 'SELECT * FROM notification WHERE email = $1 OR whatsapp_number = $2';
+    const checkValues = [email, whatsapp_number];
+    const checkResult = await pool.query(checkQuery, checkValues);
+
+    // If email or whatsapp_number already exists, return an error
+    if (checkResult.rows.length > 0) {
+        throw new Error('Email or whatsapp number already exists');
+    }
+
+    // If not, proceed with insertion
     const insertQuery = 'INSERT INTO notification (email, whatsapp_number, name) VALUES ($1, $2, $3) RETURNING *';
     const insertValues = [email, whatsapp_number, name];
     const result = await pool.query(insertQuery, insertValues);
     return result.rows[0];
-  }
+}
 
   static async getAll() {
     const query = 'SELECT * FROM notification ORDER BY id DESC';

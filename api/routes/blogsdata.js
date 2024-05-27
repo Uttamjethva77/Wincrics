dc3d56efeb931bc1118855454f64f128ce474a85
Uttam Jethva <95206154+Uttamjethva77@users.noreply.github.com>
2024-05-27 17:32:45 +0530

    Update blogsdata.js

--- api/routes/blogsdata.js
+++ api/routes/blogsdata.js
@@ -1,15 +1,31 @@
-// In your blogsdata.js route file
 const express = require('express');
-const { getAllBlogsData } = require('../controller/Blogsdata');
-
+const { getAllBlogsData, getBlogById } = require('../controller/Blogsdata');
+const { logAnalytics } = require('../middelwear/analytics')
 const blogdata = express.Router();
-
+blogdata.use(logAnalytics);
+// Route to get all blogs data
 blogdata.get('/', async (req, res) => {
   try {
     const blogsData = await getAllBlogsData();
     res.json(blogsData);
   } catch (error) {
     console.error('Error fetching blogs data:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+// Route to get a blog by its ID with selected fields
+blogdata.get('/:id', async (req, res) => {
+  const blogId = req.params.id;
+
+  try {
+    const blog = await getBlogById(blogId);
+    if (!blog) {
+      return res.status(404).json({ message: 'Blog not found' });
+    }
+    res.json(blog);
+  } catch (error) {
+    console.error('Error fetching blog by ID:', error);
     res.status(500).json({ message: 'Internal server error' });
   }
 });
